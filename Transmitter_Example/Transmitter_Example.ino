63969064c1e623de4f8d2e805e94da7ba9d37744
jinbest17 <47708424+jinbest17@users.noreply.github.com>
2019-02-24 16:11:02 -0800

    Update Transmitter_Example.ino

--- Transmitter_Example/Transmitter_Example.ino
+++ Transmitter_Example/Transmitter_Example.ino
@@ -15,6 +15,7 @@ RF24 radio(7,8);
 byte addresses[][6] = {"1Node","2Node"};
 // Used to control whether this node is sending or receiving
 bool role = 1;
+bool data = 0;
 void setup() {
   Serial.begin(115200);
   Serial.println(F("RF24/examples/GettingStarted"));
@@ -41,80 +42,25 @@ void loop() {
   
   
 /****************** Ping Out Role ***************************/  
-if (role == 1)  {
     
     radio.stopListening();                                    // First, stop listening so we can talk.
     
     
     Serial.println(F("Now sending"));
     unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &start_time, sizeof(unsigned long) )){
+     if (!radio.write( &data, sizeof(bool) )){
        Serial.println(F("failed"));
      }
         
     radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
         
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent "));
-        Serial.print(start_time);
-        Serial.print(F(", Got response "));
-        Serial.print(got_time);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
+    if(data == 0){
+      data =1;
+    }else {
+      data = 0;
     }
     // Try again 1s later
     delay(1000);
-  }
-/****************** Pong Back Role ***************************/
-  if ( role == 0 )
-  {
-    unsigned long got_time;
-    
-    if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-   }
- }
-/****************** Change Roles via Serial Commands ***************************/
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
-    
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
-    }
-  }
+
+
 } // Loop
